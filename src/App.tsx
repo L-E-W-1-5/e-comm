e6e8c450f318e3a9a782f8d7fe867750081b3c65
constantine30 <banciu.constantin@gmail.com>
2023-05-03 13:49:22 +0100

    maybe routes done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import './App.css';
+import HomePage from './Components/HomePage/HomePage';
+import SearchPage from './Components/SearchPage/SearchPage';
+import ContactUs from './Components/ContactUs/ContactUs';
+import AppInfo from './Components/AppInfo/AppInfo';
 
 
 
@@ -7,9 +12,14 @@ function App() {
 
   return (
 
-
-    <div>MotoMarket</div>
-
+    <Router>
+      <Routes>
+        <Route path='/' Component={HomePage}></Route>
+        <Route path='/SearchPage' Component={SearchPage}></Route>
+        <Route path='/ContactUs' Component={ContactUs}>Hello</Route>
+        <Route path='/AppInfo' Component={AppInfo}></Route>
+      </Routes>
+    </Router>
   
   )
 }
---
+++ src/Components/AppInfo/AppInfo.tsx
@@ -0,0 +1,3 @@
+export default function AppInfo (){
+    return <>AppInfo</>
+}
