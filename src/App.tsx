03bd032b05f200eef1cc77676fce18cdefefcddc
L-E-W-1-5 <lewiswootton88@hotmail.com>
2023-05-03 11:52:48 +0100

    auth0 installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import {Auth0Provider} from '@auth0/auth0-react';
+
 
 function App() {
-  return <body>MotoMarket</body>
-   
+
+  return (
+  <Auth0Provider
+    domain="dev-otxf3y3m35xq561z.uk.auth0.com"
+    clientId="OeE2UEHKTRF43HqCQnELYZ01x1sTAjXX"
+  >
+
+    <div>MotoMarket</div>
+
+  </Auth0Provider>
+  )
 }
 
 export default App;
