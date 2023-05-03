7ce09071fb59b99bc13beeda03f514a7ab262f05
L-E-W-1-5 <lewiswootton88@hotmail.com>
2023-05-03 12:46:50 +0100

    moved auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import './App.css';
-import {Auth0Provider} from '@auth0/auth0-react';
+
 
 
 function App() {
 
   return (
-  <Auth0Provider
-    domain="dev-otxf3y3m35xq561z.uk.auth0.com"
-    clientId="OeE2UEHKTRF43HqCQnELYZ01x1sTAjXX"
-  >
+
 
     <div>MotoMarket</div>
 
-  </Auth0Provider>
+  
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+
+    <Auth0Provider
+      domain="dev-otxf3y3m35xq561z.uk.auth0.com"
+      clientId="OeE2UEHKTRF43HqCQnELYZ01x1sTAjXX"
+    >
+
+      <App />
+
+    </Auth0Provider>
+
   </React.StrictMode>
 );
 
